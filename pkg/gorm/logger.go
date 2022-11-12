6cdcfe47f8331d82f45fb33814cd5e05eeec006a
Alex Lukashevich <aleksandr.dwt@gmail.com>
2022-11-12 14:00:40 +0200

    Gorm logger: add zap

--- pkg/gorm/logger.go
+++ pkg/gorm/logger.go
@@ -1,26 +1,26 @@
 package gorm
 
 import (
-	"log"
-	"os"
-
 	"gorm.io/gorm/logger"
+
+	"github.com/matrixbotio/go-common-lib/pkg/zes"
 )
 
-func NewLogger(debugMode bool) logger.Interface {
+func NewLogger(loggersFactory *zes.Logger, debugMode bool) logger.Interface {
 	var lvl logger.LogLevel
 	if debugMode {
 		lvl = logger.Info
 	} else {
 		lvl = logger.Silent
 	}
+	writer := newWriter(loggersFactory)
 
 	return logger.New(
-		log.New(os.Stdout, logger.Green+"GORM "+logger.Reset, log.Ldate|log.Lmicroseconds|log.Lmsgprefix),
+		writer,
 		logger.Config{
 			LogLevel:                  lvl,
 			IgnoreRecordNotFoundError: true,
-			Colorful:                  true,
+			Colorful:                  false,
 		},
 	)
 }
