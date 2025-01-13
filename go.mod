e8f34928f6379dd91bf912ce4d5653d837204429
Sagleft <sagleft@inbox.ru>
2025-01-13 15:15:56 +0300

    add new nano ID generator

--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
 module github.com/matrixbotio/go-common-lib
 
-go 1.17
+go 1.21
 
 require (
 	github.com/elastic/go-elasticsearch/v7 v7.13.1
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/matoous/go-nanoid v1.5.1
 	github.com/matrixbotio/constants-lib v1.0.87
 	github.com/stretchr/testify v1.8.0
 	go.uber.org/zap v1.22.0
@@ -15,7 +16,6 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/jaevor/go-nanoid v1.4.0 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
--- pkg/nano/nano.go
+++ pkg/nano/nano.go
@@ -2,15 +2,58 @@ package nano
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
-	"github.com/jaevor/go-nanoid"
+	gonanoid "github.com/matoous/go-nanoid"
 )
 
-func ID() string {
-	id, err := nanoid.Standard(11)
-	if err != nil {
-		fmt.Println(err)
-		return ""
+const nanoIDLen = 11
+
+func ID(length ...int) string {
+	genLength := nanoIDLen
+	if len(length) > 0 {
+		genLength = length[0]
 	}
-	return id()
+
+	id, err := gonanoid.ID(genLength)
+	if err != nil {
+		fmt.Println("gen nano ID:", err)
+		if genLength < 0 {
+			genLength = nanoIDLen
+		}
+
+		id = GetRandomString(genLength)
+	}
+	return id
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+// GetRandomString - superfast string generation.
+// 139 ns/op   32 B/op   2 allocs/op.
+// topic: https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
+func GetRandomString(length int) string {
+	var rndSource = rand.NewSource(time.Now().UnixNano())
+
+	b := make([]byte, length)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := length-1, rndSource.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = rndSource.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
 }
