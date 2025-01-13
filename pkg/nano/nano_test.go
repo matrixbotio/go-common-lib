48c99c0f3841801db9d50559cf32d555bd255540
Sagleft <sagleft@inbox.ru>
2025-01-13 21:07:59 +0300

    add new nano ID generator (#44)
    * add new nano ID generator
    
    * update test

---
+++ pkg/nano/nano_test.go
@@ -0,0 +1,34 @@
+package nano
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIDWithLength(t *testing.T) {
+	// when
+	result := ID(11)
+
+	// then
+	assert.NotEmpty(t, result)
+	assert.Len(t, result, 11)
+}
+
+func TestIDWithoutLength(t *testing.T) {
+	// when
+	result := ID()
+
+	// then
+	assert.NotEmpty(t, result)
+	assert.Len(t, result, 11)
+}
+
+func TestIDInvalidLength(t *testing.T) {
+	// when
+	result := ID(-5)
+
+	// then
+	assert.NotEmpty(t, result)
+	assert.Len(t, result, 11)
+}
